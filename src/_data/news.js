9735f82770a4de93bcdd3b9a9603554ec860f30e
Yordan Ivanov <mail@ivanovyordan.com>
2020-12-29 19:43:58 +0200

    Add localized news by country

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,31 @@
 const axios = require("axios");
 require("dotenv").config();
 
-module.exports = async function () {
+const countries = require("./countries.json");
+
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/top-headlines?country=bg&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`
+      `http://newsapi.org/v2/top-headlines?country=${country}&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`
     );
 
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
   } catch (error) {
+    return {
+      country: country,
+      articles: [],
+    };
     console.log(error);
   }
+}
+
+module.exports = async function () {
+  const newsPromises = countries.map(getNews);
+
+  return Promise.all(newsPromises).then((newsObjects) => {
+    return [].concat.apply([], newsObjects);
+  });
 };
