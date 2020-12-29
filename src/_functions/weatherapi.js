7b26bd8d0840bdb7fd99d38974b9aa79e7011cfe
Yordan Ivanov <mail@ivanovyordan.com>
2020-12-29 22:30:56 +0200

    Fix weather units

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.handler = async function (event, _, callback) {
   const { lat, lon } = event.queryStringParameters;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&unit=metric&appid=${process.env.WEATHER_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
   const response = await axios.get(url);
 
   callback(null, {
